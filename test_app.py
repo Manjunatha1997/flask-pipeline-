9246a4c5c224359f343e29ceb1c286fecf4776e7
Manjunatha1997 <mr.papireddigari@gmail.com>
2026-04-30 17:10:29 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+def test_home_status_code(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_home_content(client):
+    response = client.get("/")
+    assert b"Hello, World!" in response.data
